9ff272c99a82ea0124a4a0f18c3b328048e08649
Lucas Jablon <lucas.jablon@gmail.com>
2023-12-24 14:11:01 -0800

    format with prettier and UI initial build

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,58 +1,83 @@
-import { View, Text, SafeAreaView, Image, TextInput, ScrollView} from 'react-native';
-import React, { useLayoutEffect } from 'react';
-import {useNavigation} from "@react-navigation/native";
 import {
-    UserIcon,
-    ChevronDownIcon,
-    MagnifyingGlassIcon,
-    AdjustmentsVerticalIcon
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
-import Categories  from '../components/Categories';
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 export default function HomeScreen() {
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        });
-    }, []);
-  
-    return (
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
     <SafeAreaView className="bg-white pt-5">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image 
-              source={{
-                uri: "https://links.papareact.com/wru"
-              }}
-              className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3 rounded-md">
+          <MagnifyingGlassIcon color="#00CCBB" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
           />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">
-                Current Location
-                <ChevronDownIcon size={20} color="#00CCBB"/>
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB"/>
         </View>
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-          <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3 rounded-md">
-            <MagnifyingGlassIcon color="#00CCBB"/>
-            <TextInput placeholder='Restaurants and cuisines' keyboardType='default'/>
-          </View>
-          <AdjustmentsVerticalIcon color="#00CCBB"/>
-        </View>
-        
-        {/* Body */}
-        <ScrollView className="bg-gray-100">
-          {/* Categories */}
-          <Categories/>
-          
-          {/* Featured Row */}
-          
-        </ScrollView>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured Rows*/}
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone has been enjoying these tasty discounts!"
+        />
+        <FeaturedRow
+          id="12345"
+          title="Offers near you!"
+          description="Tasty discounts for you!"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
